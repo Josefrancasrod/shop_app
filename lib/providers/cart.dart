7fd4e32dc853f9c98e18b69037a77fb75f3722d5
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2020-05-24 23:05:48 -0500

    Functionality of favorite button and a items filter to show only favorites or all items

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -17,15 +17,32 @@ class CartItem {
 class Cart with ChangeNotifier {
   Map<String, CartItem> _items;
 
-  Map<String, CartItem> get items{
+  Map<String, CartItem> get items {
     return {...items};
   }
 
-  void addItem(String productId, double price, String title){
-    if(_items.containsKey(productId)){
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
       //hcnage quantity...
-    }else{
-      _items.putIfAbsent(productId, () => CartItem(id: DateTime.now().toString(), title: title, price: price, ),)
+      _items.update(
+        productId,
+        (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          quantity: existingCartItem.quantity + 1,
+          price: existingCartItem.price,
+        ),
+      );
+    } else {
+      _items.putIfAbsent(
+        productId,
+        () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
     }
   }
 }
